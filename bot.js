fef58c5a900be1c958ef41f0e6ac2ff8bf6cb63e
typ99 <superarkada@gmail.com>
2026-06-16 00:18:28 +0300

    init

---
+++ bot.js
@@ -0,0 +1,45 @@
+import { Bot, InputFile } from "grammy";
+import { translate, speak } from 'google-translate-api-x';
+import pkg from '@romanize/korean';
+import 'dotenv/config';
+
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const bot = new Bot(BOT_TOKEN);
+const { romanize } = pkg;
+
+const getTranslationAndAudio = async (text) => {
+    try {
+        const translationResult = await translate(text, { from: 'ru', to: 'ko' });
+        const koreanText = translationResult.text;
+        const audioBase64 = await speak(koreanText, { to: 'ko' });
+        const audioBuffer = Buffer.from(audioBase64, 'base64');
+
+        return { koreanText, audioBuffer };
+    } catch (error) {
+        console.error("Ошибка при переводе или получении аудио:", error);
+
+        return null;
+    }
+};
+
+bot.command("start", (ctx) => ctx.reply("🇷🇺🇰🇷 Привет! Я ПереКор: перевожу на корейский, даю транскрипцию и произношение. Попробуй: напиши «спасибо»."));
+bot.on("message:text", async (ctx) => {
+    const userText = ctx.message.text;
+    await ctx.reply(`🔍 Обрабатываю запрос: "${userText}"...`);
+
+    const result = await getTranslationAndAudio(userText);
+
+    if (!result) {
+        await ctx.reply("❌ Не удалось перевести текст. Попробуйте позже.");
+        return;
+    }
+
+    const transcription = romanize(result.koreanText);
+
+    await ctx.reply(`✅ Перевод (хангыль): ${result.koreanText}\n📖 Транскрипция (латиницей): ${transcription}`);
+    await ctx.replyWithVoice(new InputFile(result.audioBuffer), {
+        caption: "🎙️ Произношение слова на корейском",
+    });
+});
+
+bot.start();
